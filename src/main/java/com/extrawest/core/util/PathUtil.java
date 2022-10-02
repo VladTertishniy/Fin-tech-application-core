8202f626f13dfeb9f37f0337ede8e863c8e07bc5
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-03 02:52:04 +0300

    added utility class

---
+++ src/main/java/com/extrawest/core/util/PathUtil.java
@@ -0,0 +1,13 @@
+package com.extrawest.core.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PathUtil {
+    public static final String LOANS_ROOT_PATH = "/loans";
+    public static final String APPLICATION_FORMS_ROOT_PATH = "/applicationForms";
+    public static final String CREATE_PATH = "/create";
+    public static final String UPDATE_PATH = "/update/{id}";
+    public static final String DELETE_PATH = "/delete/{id}";
+    public static final String ACCEPT_PATH = "/accept/{id}";
+}
